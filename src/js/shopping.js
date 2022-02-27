fbcdb43ee60a8646fede806e002422cc113ae7d1
evertonlopesc <everton.lopess@outlook.com>
2022-02-27 17:24:49 -0300

    feat: shopping.js created.

---
+++ src/js/shopping.js
@@ -0,0 +1,41 @@
+var shopping = new Vue ({
+  el: '#shopping',
+  data: {
+    header: 'Mini Kalzone',
+    image: './img/prod_1.jpeg',
+    details: [
+      'Name: XXX', 'Ingredient: ...'
+    ],
+    items: [
+      {
+        prodId: 1,
+        product: 'Product 1',
+        productImage: '../img/prod_1.jpeg'
+      },
+      {
+        prodId: 2,
+        product: 'Product 2',
+        productImage: '../img/prod_2.jpg'
+      },
+      {
+        prodId: 3,
+        product: 'Product 3',
+        productImage: '../img/prod_3.jpg'
+      },
+      {
+        prodId: 4,
+        product: 'Product 4',
+        productImage: '../img/mega-sem-nocao.jpg'
+      },
+    ],
+    cart: 0,
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    cleanToCart() {
+      this.cart = 0
+    },
+  },
+})
