4cfaee2424b78699ab3b9993d40d2a3bb1a91023
evertonlopesc <everton.lopess@outlook.com>
2022-02-28 11:53:05 -0300

    fix: images path.

--- src/js/shopping.js
+++ src/js/shopping.js
@@ -9,23 +9,23 @@ var shopping = new Vue ({
     items: [
       {
         prodId: 1,
-        product: 'Product 1',
-        productImage: '../img/prod_1.jpeg'
+        product: "Product 1",
+        productImage: "./img/prod_1.jpeg"
       },
       {
         prodId: 2,
-        product: 'Product 2',
-        productImage: '../img/prod_2.jpg'
+        product: "Product 2",
+        productImage: "./img/prod_2.jpg"
       },
       {
         prodId: 3,
-        product: 'Product 3',
-        productImage: '../img/prod_3.jpg'
+        product: "Product 3",
+        productImage: "./img/prod_3.jpg"
       },
       {
         prodId: 4,
-        product: 'Product 4',
-        productImage: '../img/mega-sem-nocao.jpg'
+        product: "Product 4",
+        productImage: "./img/mega-sem-nocao.jpg"
       },
     ],
     cart: 0,
